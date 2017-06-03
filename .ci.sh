3d8eb0b3658c9bd34984ba736e77e6e2ea8ccffc
Zach Deibert <zachdeibert@gmail.com>
2017-06-03 18:10:11 -0500

    Back to xbuild

--- .ci.sh
+++ .ci.sh
@@ -2,8 +2,8 @@
 set -e
 
 # Build
-msbuild /t:CoreCompile /p:Configuration=Prerelease /p:SolutionDir=$(pwd) *.csproj
+xbuild /t:CoreCompile /p:Configuration=Prerelease /p:SolutionDir=$(pwd) *.csproj
 
 # Deploy
 git fetch --unshallow # Fixes version number generation for more than 50 commits
-msbuild /t:CIBuild /p:Configuration=Prerelease /p:SolutionDir=$(pwd) *.csproj
+xbuild /t:CIBuild /p:Configuration=Prerelease /p:SolutionDir=$(pwd) *.csproj
