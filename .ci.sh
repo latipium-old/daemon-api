0525eb630b1686ff57c9e30ff73945ec5884bfff
Zach Deibert <zachdeibert@gmail.com>
2017-06-03 18:29:33 -0500

    Changed build commands (again)

--- .ci.sh
+++ .ci.sh
@@ -2,8 +2,8 @@
 set -e
 
 # Build
-xbuild /t:CoreCompile /p:Configuration=Prerelease /p:SolutionDir=$(pwd) *.csproj
+msbuild /p:Configuration=Prerelease /p:SolutionDir=$(pwd) *.csproj
 
 # Deploy
 git fetch --unshallow # Fixes version number generation for more than 50 commits
-xbuild /t:CIBuild /p:Configuration=Prerelease /p:SolutionDir=$(pwd) *.csproj
+msbuild /t:CIBuild /p:Configuration=Prerelease /p:SolutionDir=$(pwd) *.csproj
